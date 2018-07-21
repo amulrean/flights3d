486ffcbe38ea2ed3fa7d3c1feee50bec7cc28830
Andrew Mulrean <andrew@thehumangeo.com>
2018-07-21 11:52:46 -0400

    Add Cesium to Project

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  viewer;
+
   constructor() { }
 
   ngOnInit() {
+    // tslint:disable-next-line:max-line-length
+    Cesium.Ion.defaultAccessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2Y2VlNTZiMy04YzE5LTQ3OWYtYmQ0MC03NGZlODdlZmJhMDYiLCJpZCI6MjI0NCwiaWF0IjoxNTMyMTg3Mzc1fQ.6E2ATk75Gdk9uzyuTPd-QcHKksPxfqx82wifY2zJ5P0`;
+    this.viewer = new Cesium.Viewer('cesiumContainer', {
+      sceneMode: Cesium.SceneMode.SCENE3D,
+    });
+
+    const tileset = this.viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+        url : 'https://s3.amazonaws.com/amulrean-vricon/usa/washington_dc/tileset.json'
+    }));
+    this.viewer.zoomTo(tileset);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,7 @@ if (environment.production) {
   enableProdMode();
 }
 
+window['CESIUM_BASE_URL'] = '/assets/cesium';
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
