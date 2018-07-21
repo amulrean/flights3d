29506ad0db19e114440084c250de57adbead9d72
Andrew Mulrean <andrew@thehumangeo.com>
2018-07-21 11:18:16 -0400

    Add Map and Side bar and switch to scss.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppCustomMaterialImportsModule} from './app-custom-material-imports.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import {MapComponent} from './map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
